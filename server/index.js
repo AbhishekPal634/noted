96bc2e0a6963f5dbf9ed8e31608aab20be3ecf8a
Abhishek Pal <abhishekpal1413@gmail.com>
2024-10-23 09:07:02 +0530

    bug fixes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
-const  cors = require("cors");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const axios = require("axios");
 
 const app = express();
 const PORT = 8800;
@@ -10,20 +11,42 @@ const PORT = 8800;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/pages/index.html"));
+  res.sendFile(path.join(__dirname, "../client/pages/index.html"));
 });
 
 app.get("/userDashboard/:id", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/pages/task.html"));
+  res.sendFile(path.join(__dirname, "../client/pages/task.html"));
 });
 
 app.use("/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+const url = `https://noted-8mnl.onrender.com`;
+const interval = 30000;
 
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
